0bd1832b2a41848ba8348c4f2a912ff6ee10123b
ViperEkura <3081035982@qq.com>
2025-01-17 12:57:22 +0800

    修改了模型采样的方式

--- module/generate.py
+++ module/generate.py
@@ -31,15 +31,31 @@ class Khaosz:
         self.model = Transformer(self.config)
         self.model.load_state_dict(torch.load(weight_path, weights_only=True))
         
-        
-    def loop_impl(self, ids, temperature):
+    def sample_next_token(self, ids, temperature=1.0, top_k=0, top_p=0.0, filter_value=-float('Inf')):
         device = next(self.model.parameters()).device
         input_tensor = torch.tensor(ids, device=device).unsqueeze(0)
-        prob = self.model(input_tensor)[-1, -1, :]
-        prob = torch.softmax(prob / temperature, dim=-1)
+        logits = self.model(input_tensor)[-1, -1, :] / temperature
         
-        next_token_id = torch.multinomial(prob, num_samples=1, replacement=True).item()
+        top_k = min(top_k, logits.size(-1))
+        if top_k > 0:
+            indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+            logits[indices_to_remove] = filter_value
+
+        if top_p > 0.0:
+            sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+            cumulative_probs = torch.cumsum(torch.softmax(sorted_logits, dim=-1), dim=-1)
+
+            sorted_indices_to_remove = cumulative_probs > top_p
+            sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+            sorted_indices_to_remove[..., 0] = 0
+
+            indices_to_remove = sorted_indices[sorted_indices_to_remove]
+            logits[indices_to_remove] = filter_value
+            
+        probabilities = torch.softmax(logits, dim=-1)
+        next_token_id = torch.multinomial(probabilities, num_samples=1,replacement=True).item()
         next_token = self.tokenizer.id_to_token(next_token_id)
+        
         return next_token, next_token_id
     
     def stream_generate(
@@ -47,8 +63,8 @@ class Khaosz:
             query: str, 
             history: list[tuple[str, str]]=None,
             temperature: float=1.0,
-            top_k: int=10,
-            top_p: float=0.8,
+            top_k: int=0,
+            top_p: float=0.0,
         ):
         if history is None:
             history = list()
@@ -59,7 +75,10 @@ class Khaosz:
         
         self.model.eval()
         while len(ids) < self.config.m_len:
-            next_token, next_token_id = self.loop_impl(ids, temperature)
+            next_token, next_token_id = self.sample_next_token(
+                ids, temperature, 
+                top_k=top_k, top_p=top_p
+            )
             if next_token == "<eog>":
                 break    
             response += next_token
@@ -82,7 +101,10 @@ class Khaosz:
             
             self.model.eval()
             while len(ids) < self.config.m_len:
-                next_token, next_token_id = self.loop_impl(ids, temperature)
+                next_token, next_token_id = self.sample_next_token(
+                    ids, temperature, 
+                    top_k=top_k, top_p=top_p
+                )
                 if next_token == "<eog>":
                     break
                 
