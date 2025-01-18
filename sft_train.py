fdf23f73f8336baae85831e9b02a565361e07750
ViperEkura <3081035982@qq.com>
2025-01-19 02:22:18 +0800

    增加共享权重， 改进参数大小

--- sft_train.py
+++ sft_train.py
@@ -1,4 +1,11 @@
-# from module import *
+from module import *
 
-# model = Transformer(Config('params/config.json'))
-# print(f"model parameter size: {model.parameter_size():,}")
+model = Transformer(Config('params/config.json'))
+print(f"model parameter size: {model.parameter_size():,}")
+
+bsz = 1
+n_dim = 1024
+m_len = 512
+
+x = torch.randint(0, 100, size=(bsz, m_len))
+y = model(x)
