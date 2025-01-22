cda852bfdc46b8846311fdbe3699b3099262ab1c
ViperEkura <3081035982@qq.com>
2025-01-22 22:28:04 +0800

    修改了生成大小设置的错误

--- chat.py
+++ chat.py
@@ -7,23 +7,24 @@ warnings.filterwarnings("ignore")
 def chat():
     model = Khaosz("params")
     model = model.to(device='cuda', dtype=torch.bfloat16)
-    response_size, histroy = 0, []
+    histroy = []
 
     while True:
         query = input(">> ")
         if query == "!exit":
             break
         
+        response_size = 0
         for response, histroy in model.stream_generate(
             query=query, 
             history=histroy,
-            temperature=1.2,
-            top_k=50,
+            temperature=1.0,
             top_p=0.8
         ):
             print(response[response_size:], end="")
-            response_size = len(response)     
+            response_size = len(response)       
         print()
+
 
 def show_parameter_size():
     cfg = Config("params/config.json")
