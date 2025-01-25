5ddab44c0f08e61652edec0694efcaf1fb689f4c
ViperEkura <3081035982@qq.com>
2025-01-25 12:21:05 +0800

    修改生成参数

--- chat.py
+++ chat.py
@@ -19,7 +19,7 @@ def chat():
             query=query, 
             history=histroy,
             temperature=1.0,
-            top_p=0.5
+            top_p=0.3
         ):
             print(response[response_size:], end="")
             response_size = len(response)       
