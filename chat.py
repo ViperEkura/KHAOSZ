7c1041d4c0737434fb8c55fd88065a7dbd836f67
ViperEkura <3081035982@qq.com>
2025-01-25 18:58:31 +0800

    增加 with torch.no_grad()

--- chat.py
+++ chat.py
@@ -8,22 +8,22 @@ def chat():
     model = Khaosz("params")
     model = model.to(device='cuda', dtype=torch.bfloat16)
     histroy = []
-
-    while True:
-        query = input(">> ")
-        if query == "!exit":
-            break
-        
-        response_size = 0
-        for response, histroy in model.stream_generate(
-            query=query, 
-            history=histroy,
-            temperature=1.0,
-            top_p=0.3
-        ):
-            print(response[response_size:], end="")
-            response_size = len(response)       
-        print()
+    with torch.no_grad():
+        while True:
+            query = input(">> ")
+            if query == "!exit":
+                break
+            
+            response_size = 0
+            for response, histroy in model.stream_generate(
+                query=query, 
+                history=histroy,
+                temperature=1.0,
+                top_p=0.3
+            ):
+                print(response[response_size:], end="", flush=True)
+                response_size = len(response)       
+            print()
 
 
 def show_parameter_size():
