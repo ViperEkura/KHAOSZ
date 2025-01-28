6adeaf198d753fec89bcd039f6cbd0b68266b25c
ViperEkura <3081035982@qq.com>
2025-01-28 13:45:37 +0800

    备份优化器

--- trainer/optimizer.py
+++ trainer/optimizer.py
@@ -69,3 +69,47 @@ class MixedPrecisionOptimizer(Optimizer):
     def step(self):
         pass
     
+    
+class CustomAdamW(Optimizer):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=1e-2):
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        super(CustomAdamW, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+
+                grad = p.grad.data
+                state = self.state[p]
+
+                # 初始化状态
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p.data, dtype=torch.bfloat16)
+                    state['exp_avg_sq'] = torch.zeros_like(p.data, dtype=torch.bfloat16)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+                bias_correction1 = 1 - beta1 ** state['step']
+                bias_correction2 = 1 - beta2 ** state['step']
+
+                denom = (exp_avg_sq.to(torch.bfloat16).sqrt() / (bias_correction2 ** 0.5)).add_(group['eps'])
+                step_size = group['lr'] / bias_correction1
+
+                p.data.addcdiv_(exp_avg.to(torch.bfloat16), denom, value=-step_size)
+
+                if group['weight_decay'] != 0:
+                    p.data.add_(p.data, alpha=-group['lr'] * group['weight_decay'])
+
+        return loss
