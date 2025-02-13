52ede55e14a1c2a60d1c0748d874caad1073b3a8
ViperEkura <3081035982@qq.com>
2025-02-13 19:13:06 +0800

    修改生成内容

--- batch_generate.py
+++ batch_generate.py
@@ -6,14 +6,15 @@ warnings.filterwarnings("ignore")
 def batch_generate():
     model = Khaosz("params")
     model = model.to(device='cuda', dtype=torch.float16)
-    queries = ["什么是人工智能", "什么是高性能计算"]
-    response = model.batch_generate(
+    queries = ["什么是人工智能", "什么是高性能计算", "什么是transformer"]
+    responses = model.batch_generate(
         queries=queries,
         temperature=0.9,
         top_k=50,
         top_p=0.6,
     )
-    print(response)
+    for response in responses:
+        print(response)
     
 if __name__  == "__main__":
      batch_generate()
