732e0e0e091c4fe8287e860826d6c5289cd49da3
ViperEkura <3081035982@qq.com>
2025-02-16 13:42:25 +0800

    增加dpo数据生成

--- chat.py
+++ chat.py
@@ -17,9 +17,8 @@ def chat():
         for response, histroy in model.stream_generate(
             query=query, 
             history=histroy,
-            temperature=0.8,
+            temperature=0.9,
             top_p=0.8,
-            top_k=50
         ):
             print(response[response_size:], end="", flush=True)
             response_size = len(response)       
--- generate.py
+++ generate.py
@@ -6,11 +6,12 @@ warnings.filterwarnings("ignore")
 
 def batch_generate(
     queries,
+    model_dir="params",
     temperature=0.95, 
     top_k=50, 
-    top_p=0.8
+    top_p=0.8 
     ):
-    model = Khaosz("params")
+    model = Khaosz(model_dir)
     model = model.to(device='cuda', dtype=torch.float16)
     responses = [
         model.generate(
@@ -22,11 +23,7 @@ def batch_generate(
         ]
     outputs = []
     for query, response in zip(queries, responses):
-        out_dict = {
-            "query": query,
-            "response": response
-        }
-        outputs.append(out_dict)
+        outputs.append({"query": query, "response": response})
     return outputs
 
 def dpo_generate(input_json_file):
@@ -35,7 +32,25 @@ def dpo_generate(input_json_file):
     with open(input_json_file, "r") as f:
         json_file = json.loads(f)
 
+    item_size = len(json_file)
+    queries = [item["question"] for item in json_file]
+    recepted = [item['recepted'] for item in json_file]
+    del json_file
     
+    rejected = batch_generate(queries)
+    output_dict = []
+    
+    for i in range(item_size):
+        output_dict.append({
+            "question": queries[i],
+            "recepted": recepted[i],
+            "rejected": rejected[i]
+        })
+
+    return output_dict
+
+def generate_dpo_data(dpo_dict):
+    pass
 
     
 if __name__  == "__main__":
