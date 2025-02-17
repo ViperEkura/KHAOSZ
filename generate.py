8d58706a5d7ee09e413a5145be028c451d87f653
ViperEkura <3081035982@qq.com>
2025-02-17 15:18:52 +0800

    修改dpo

--- generate.py
+++ generate.py
@@ -1,18 +1,20 @@
-import torch
+import os
 import json
+import torch
 import warnings
+
 from module import Khaosz
+from typing import List, Dict
+
 warnings.filterwarnings("ignore")
 
 def batch_generate(
     queries,
-    model_dir="params",
+    model: Khaosz,
     temperature=0.95, 
     top_k=50, 
     top_p=0.8 
-    ):
-    model = Khaosz(model_dir)
-    model = model.to(device='cuda', dtype=torch.float16)
+) -> Dict:
     responses = [
         model.generate(
             query=query, 
@@ -22,22 +24,23 @@ def batch_generate(
             ) for query in queries
         ]
     outputs = []
-    for query, response in zip(queries, responses):
-        outputs.append({"query": query, "response": response})
+    for response in responses:
+        outputs.append(response)
     return outputs
 
-def dpo_generate(input_json_file):
-    model = Khaosz("params")
-    
+def dpo_generate(
+    model: Khaosz,
+    input_json_file: str,
+    question_key: str="question",
+    recepted_key: str="recepted",
+)-> List:  
     with open(input_json_file, "r") as f:
         json_file = json.loads(f)
-
-    item_size = len(json_file)
-    queries = [item["question"] for item in json_file]
-    recepted = [item['recepted'] for item in json_file]
-    del json_file
+        item_size = len(json_file)
+        queries = [item[question_key] for item in json_file]
+        recepted = [item[recepted_key] for item in json_file]
     
-    rejected = batch_generate(queries)
+    rejected = batch_generate(queries, model)
     output_dict = []
     
     for i in range(item_size):
@@ -49,9 +52,27 @@ def dpo_generate(input_json_file):
 
     return output_dict
 
-def generate_dpo_data(dpo_dict):
-    pass
-
+def dpo(
+    model: Khaosz,
+    input_json_file: str,
+    output_json_file: str,
+    question_key: str="question",
+    recepted_key: str="recepted",
+):
+    output_dict = dpo_generate(model, input_json_file, question_key, recepted_key)
     
+    with open(output_json_file, "w") as f:
+        json.dump(output_dict, f)
+
+
 if __name__  == "__main__":
-     pass
+     model = Khaosz("params")
+     model = model.to(device='cuda', dtype=torch.float16)
+     
+     dpo(
+         model,
+         input_json_file="data/dpo_data.json",
+         output_json_file="data/dpo_output.json",
+         question_key="question",
+         recepted_key="recepted"
+     )
