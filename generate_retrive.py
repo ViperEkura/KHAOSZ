0c2878dcf447c36dafc470daaddf7f963bc3240d
ViperEkura <3081035982@qq.com>
2025-06-03 10:57:47 +0800

    修改检索模块

---
+++ generate_retrive.py
@@ -0,0 +1,13 @@
+from module import Khaosz
+import torch
+
+
+if __name__ == "__main__":
+    model = Khaosz("params")
+    model = model.to(dtype=torch.bfloat16, device="cuda")
+    query = "什么是b站"
+    
+    resp = model.retrieve_generate(query, top_k=50, top_p=0.8)
+    print(resp)
+     
+     
