b9c5d5186db2b1c44b06780e2f78164b8c1f1274
ViperEkura <3081035982@qq.com>
2025-07-04 10:04:36 +0800

    修复文件错误命名

--- module/__init__.py
+++ module/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "1.1.0"
 __author__ = "ViperEkura"
 
-from .generater import *
+from .generator import *
 from .tokenizer import *
 from .transformer import *
 from .retriever import *
