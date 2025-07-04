b8475ded07d23087fc0196d92fddcb8b9d9eb1b1
ViperEkura <3081035982@qq.com>
2025-07-04 22:28:07 +0800

    refactor(module, trainer): 优化模块和训练器的导入及导出

--- khaosz/module/__init__.py
+++ khaosz/module/__init__.py
@@ -3,3 +3,16 @@ from .tokenizer import BpeTokenizer
 from .parameter import ParameterLoader
 from .generator import ChatGenerator, StreamGenerator, BatchGenerator, RetrievalGenerator
 from .retriever import Retriever, TextSplitter
+
+
+__all__ = [
+    "Transformer",
+    "BpeTokenizer",
+    "ParameterLoader",
+    "ChatGenerator",
+    "StreamGenerator",
+    "BatchGenerator",
+    "RetrievalGenerator",
+    "Retriever",
+    "TextSplitter",
+]
--- khaosz/trainer/__init__.py
+++ khaosz/trainer/__init__.py
@@ -1,3 +1,12 @@
-from .trainer import *
-from .dataset import *
-from .checkpoint import *
+from .trainer import Trainer
+from .dataset import SeqDataset, SftDataset, DpoDataset, BaseDataset
+from .checkpoint import TrainCheckPoint
+
+__all__ = [
+    "Trainer",
+    "SeqDataset",
+    "SftDataset",
+    "DpoDataset",
+    "BaseDataset",
+    "TrainCheckPoint"
+]
