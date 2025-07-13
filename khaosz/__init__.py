5bb5dff1ff9beb7accda634edc3815d5077f9d64
ViperEkura <3081035982@qq.com>
2025-07-13 21:32:57 +0800

    refactor(khaosz): 重构初始化文件并创建模型模块

--- khaosz/__init__.py
+++ khaosz/__init__.py
@@ -1,108 +1,43 @@
 __version__ = "1.2.0"
 __author__ = "ViperEkura"
 
+from khaosz.model import Khaosz
+from khaosz.module.transformer import Transformer, TransformerConfig
+from khaosz.module.retriever import Retriever, TextSplitter
+from khaosz.module.tokenizer import BpeTokenizer
+from khaosz.module.parameter import ParameterLoader, ModelParameter, RetrieverParameter
+from khaosz.module.generator import TextGenerator, ChatGenerator, StreamGenerator, BatchGenerator, RetrievalGenerator, EmbeddingEncoder
 
-from torch import Tensor
-from typing import List, Tuple, Generator, Union
+from khaosz.trainer.trainer import Trainer
+from khaosz.trainer.dataset import SeqDataset, SftDataset, DpoDataset, BaseDataset
+from khaosz.trainer.checkpoint import TrainCheckPoint
 
-from khaosz.module import *
 
-
-class Khaosz:
-    def __init__(self, model_dir: str):
-        self.parameter = ParameterLoader.load(model_dir)
+__all__ = [
+    # model
+    "Khaosz",
     
-    def to(self, *args, **kwargs):
-        self.parameter.to(*args, **kwargs)
-        return self
-
-    def generate(
-            self, 
-            query: str, 
-            history: List[Tuple[str, str]]=None,
-            temperature: float=0.8,
-            top_k: int=50,
-            top_p: float=0.95,
-        ) -> str:
-            generator = ChatGenerator(self.parameter)
-            return generator.generate(
-                query, 
-                history=history,
-                temperature=temperature, 
-                top_k=top_k, 
-                top_p=top_p,
-            )
+    # module
+    "Transformer",
+    "TransformerConfig",
+    "BpeTokenizer",
+    "ParameterLoader",
+    "ModelParameter",
+    "RetrieverParameter",
+    "TextGenerator",
+    "ChatGenerator",
+    "StreamGenerator",
+    "BatchGenerator",
+    "RetrievalGenerator",
+    "Retriever",
+    "TextSplitter",
+    "EmbeddingEncoder",
     
-    def batch_generate(
-            self, 
-            queries: List[str],
-            histories: List[Tuple[str, str]]=None,
-            temperature: float=0.8,
-            top_k: int=50,
-            top_p: float=0.95,
-        ) -> List[str]:
-            generator = BatchGenerator(self.parameter)
-            return generator.generate(
-                queries, 
-                histories=histories,
-                temperature=temperature, 
-                top_k=top_k, 
-                top_p=top_p,
-            )
-        
-    
-    def stream_generate(
-            self, 
-            query: str, 
-            history: List[Tuple[str, str]]=None,
-            temperature: float=0.8,
-            top_k: int=50,
-            top_p: float=0.95,
-        ) -> Generator[Tuple[str, List[Tuple[str, str]]], None, None]:
-            stream_generator = StreamGenerator(self.parameter)
-            return stream_generator.generate(
-                    query, 
-                    history=history,
-                    temperature=temperature, 
-                    top_k=top_k, 
-                    top_p=top_p,
-                )
-    
-    def retrieve_generate(
-            self,
-            retrieved,
-            query: str, 
-            history: List[Tuple[str, str]] = None,
-            temperature: float=0.8,
-            top_k: int=50,
-            top_p: float=0.95,
-        ) -> str:
-            generator = RetrievalGenerator(self.parameter)
-            return generator.generate(
-                retrieved,
-                query,
-                history=history,
-                temperature=temperature, 
-                top_k=top_k, 
-                top_p=top_p,
-            )
-    
-    def text_generate(            
-            self, 
-            query: str, 
-            temperature: float=0.8,
-            top_k: int=50,
-            top_p: float=0.95,
-        ) -> str:
-        generator = TextGenerator(self.parameter)
-        
-        return generator.generate(
-                query,
-                temperature=temperature, 
-                top_k=top_k, 
-                top_p=top_p,
-            )
-    
-    def encode(self, sentence: Union[str, List[str]]) -> Union[Tensor, List[Tensor]]:
-        encoder = EmbeddingEncoder(self.parameter)
-        return encoder.encode(sentence)
+    # trainer
+    "Trainer",
+    "SeqDataset",
+    "SftDataset",
+    "DpoDataset",
+    "BaseDataset",
+    "TrainCheckPoint"
+]
