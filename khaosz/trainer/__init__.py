6077df564530191ebaa3951660bcef514bfa71ec
ViperEkura <3081035982@qq.com>
2025-07-23 16:45:03 +0800

    fix(trainer): 修复模块导入bug

--- khaosz/trainer/__init__.py
+++ khaosz/trainer/__init__.py
@@ -1,12 +1,12 @@
-from .trainer import Trainer
 from .dataset import DatasetLoader
+from .trainer import Trainer, TrainCheckPoint
 from .strategy import TrainConfig, CosineScheduleConfig, SgdrScheduleConfig
 
 __all__ = [
+    "DatasetLoader",
     "Trainer",
+    "TrainCheckPoint",
     "TrainConfig",
     "CosineScheduleConfig",
     "SgdrScheduleConfig",
-    "DatasetLoader",
-    "TrainCheckPoint"
 ]
