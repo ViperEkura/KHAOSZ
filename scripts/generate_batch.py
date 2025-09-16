8c774c58b827340c014a90056be373e228a817a2
ViperEkura <3081035982@qq.com>
2025-09-16 18:41:04 +0800

    refactor(scripts): 调整生成回复的参数设置

--- scripts/generate_batch.py
+++ scripts/generate_batch.py
@@ -9,14 +9,13 @@ PROJECT_ROOT = os.path.dirname(
 def batch_generate():
     model_dir = os.path.join(PROJECT_ROOT, "params")
     model = Khaosz(model_dir).to(device='cuda', dtype=torch.bfloat16)
-    
     inputs = ["你好", "请问什么是人工智能", "今天天气如何", "我感到焦虑， 请问我应该怎么办", "请问什么是显卡"]
-
+    
     responses = model.batch_generate(
         queries=inputs,
-        temperature=0.95,
+        temperature=0.7,
         top_p=0.95,
-        top_k=50
+        top_k=30
     )
     
     for q, r in zip(inputs, responses):
