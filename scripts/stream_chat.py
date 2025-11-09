0b45e8666e71d23ac5fc666e140baa42553b1fd6
ViperEkura <3081035982@qq.com>
2025-11-09 16:30:24 +0800

    fix(scripts): 修复stream_chat.py中的拼写错误

--- scripts/stream_chat.py
+++ scripts/stream_chat.py
@@ -10,16 +10,16 @@ def chat():
     model_dir = os.path.join(PROJECT_ROOT, "params")
     model = Khaosz(model_dir).to(device='cuda', dtype=torch.bfloat16)
 
-    histroy = []
+    history = []
     while True:
         query = input(">> ")
         if query == "!exit":
             break
         
         response_size = 0
-        for response, histroy in model.stream_generate(
+        for response, history in model.stream_generate(
             query=query, 
-            history=histroy,
+            history=history,
             temperature=0.8,
             top_p=0.95,
             top_k=50
